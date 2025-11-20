f0ee2861b0ddeb7b108e82af9f634d5a32bec58c
Jun0913 <jhjh1699@gmail.com>
2025-11-20 16:08:55 +0900

    Bronze: 1000.A+B

---
+++ BaekJoon/Bronze/1000.A+B/A+B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int nA = scan.nextInt();
+        int nB = scan.nextInt();
+        System.out.println(nA+nB);
+    }
+}
